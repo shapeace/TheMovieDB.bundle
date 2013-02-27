3030800623a15c2e3d0a84c3720cdee43aabab0e
sander1 <sander@plexapp.com>
2013-02-28 00:37:12 +0100

    Cleaner include_adult check

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -96,7 +96,11 @@ class TMDbAgent(Agent.Movies):
       else:
         year = ''
 
-      tmdb_dict = self.get_json(url=TMDB_SEARCH_URL % (String.Quote(media.name), year, lang, str(Prefs['adult'])))
+      include_adult = 'false'
+      if Prefs['adult']:
+        include_adult = 'true'
+
+      tmdb_dict = self.get_json(url=TMDB_SEARCH_URL % (String.Quote(media.name), year, lang, include_adult))
 
       if tmdb_dict and 'results' in tmdb_dict:
         for i, movie in enumerate(sorted(tmdb_dict['results'], key=lambda k: k['popularity'], reverse=True)):
